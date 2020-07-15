f64035bc73645c470a8f1fceef9b81d5af4d1d53
Taha Paksu <tpaksu@gmail.com>
2020-07-16 01:11:25 +0300

    Pushed skeleton

---
+++ src/BackendServiceProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Paksuco\Backend;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackendServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->handleConfigs();
+        // $this->handleMigrations();
+        // $this->handleViews();
+        // $this->handleTranslations();
+        // $this->handleRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind any implementations.
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    private function handleConfigs()
+    {
+        $configPath = __DIR__ . '/../config/paksuco-backend.php';
+
+        $this->publishes([$configPath => base_path('config/paksuco-backend.php')]);
+
+        $this->mergeConfigFrom($configPath, 'paksuco-backend');
+    }
+
+    private function handleTranslations()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'paksuco-backend');
+    }
+
+    private function handleViews()
+    {
+        $this->loadViewsFrom(__DIR__.'/../views', 'paksuco-backend');
+
+        $this->publishes([__DIR__.'/../views' => base_path('resources/views/vendor/paksuco-backend')]);
+    }
+
+    private function handleMigrations()
+    {
+        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+    }
+
+    private function handleRoutes()
+    {
+        include __DIR__.'/../routes/routes.php';
+    }
+}
+
+if (!function_exists("base_path")) {
+    function base_path($path)
+    {
+        return \Illuminate\Support\Facades\App::basePath($path);
+    }
+}
