1801a8a14399b3231aa982c182e806e432d25610
Taha Paksu <taha.paksu@edata.com.tr>
2020-07-16 16:27:24 +0300

    Started design

--- views/header.blade.php
+++ views/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-gray-800" x-data='{open: false}' style="z-index: 10000;">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="px-2 sm:px-6 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <!-- Mobile menu button-->
@@ -32,6 +32,9 @@
                 </div>
                 <div class="hidden sm:block sm:ml-6">
                     <div class="flex">
+                        @hasSection ('navbar-top')
+                            @yield('navbar-top')
+                        @else
                         <a href="#"
                             class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
                         <a href="#"
@@ -40,6 +43,7 @@
                             class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
                         <a href="#"
                             class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -106,7 +110,10 @@
 
       Menu open: "block", Menu closed: "hidden"
     -->
-    <div x-show="open">
+    <div :class="{'block md:hidden': open, 'hidden': !open}" x-cloak>
+        @hasSection ('navbar-mobile')
+            @yield('navbar-mobile')
+        @else
         <div class="px-2 pt-2 pb-3">
             <a href="#"
                 class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
@@ -117,5 +124,6 @@
             <a href="#"
                 class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
         </div>
+        @endif
     </div>
 </nav>
--- views/layout.blade.php
+++ views/layout.blade.php
@@ -2,10 +2,10 @@
     <div class="w-full">
         @include("paksuco-backend::header")
     </div>
-    <div class="w-1/5 sidebar bg-gray-800">
+    {{--<div class="w-12 lg:relative lg:block lg:w-1/5 xl:w-1/6 sidebar bg-gray-800">
         @include("paksuco-backend::sidebar")
-    </div>
-    <div class="w-4/5 content">
+    </div>--}}
+    <div class="p-4 lg:p-0 w-full {{-- lg:w-4/5 xl:w-5/6 --}} w-full content">
         @yield('body')
     </div>
 </div>
