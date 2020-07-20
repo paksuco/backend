3a975fcf183f91a9d951c791b3b345baafa3b8b0
Taha Paksu <taha.paksu@edata.com.tr>
2020-07-20 14:49:47 +0300

    Improved responsiveness

--- views/header.blade.php
+++ views/header.blade.php
@@ -1,36 +1,36 @@
-<nav class="bg-gray-800" x-data='{open: false}' @click.away="open = false">
-    <div class="px-2 md:px-6 lg:px-8">
+<nav class="bg-gray-900">
+    <div class="px-2 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
-            <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
+            <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 <!-- Mobile menu button-->
-                <button @click="open = !open"
+                <button @click="open_sidebar = !open_sidebar"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white
                     hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
                     aria-label="Main menu" aria-expanded="false">
                     <!-- Icon when menu is closed. -->
                     <!-- Menu open: "hidden", Menu closed: "block" -->
-                    <svg class="h-6 w-6" x-show="!open" fill="none" viewBox="0 0 24 24"
+                    <svg class="h-6 w-6" x-show="!open_sidebar" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                     <!-- Icon when menu is open. -->
                     <!-- Menu open: "block", Menu closed: "hidden" -->
-                    <svg class="h-6 w-6" x-show="open" fill="none" viewBox="0 0 24 24"
+                    <svg class="h-6 w-6" x-show="open_sidebar" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
-            <div class="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
+            <div class="flex-1 flex items-center justify-center lg:items-stretch lg:justify-start">
                 <div class="flex-shrink-0 flex items-center">
                     <img class="block lg:hidden h-8 w-auto"
                         src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg" alt="Workflow logo">
                     <img class="hidden lg:block h-8 w-auto"
                         src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg" alt="Workflow logo">
                 </div>
-                <div class="hidden md:block md:ml-6">
+                <div class="hidden lg:block lg:ml-6">
                     <div x-cloak class="flex">
                         @hasSection ('navbar-top')
                             @yield('navbar-top')
@@ -47,7 +47,7 @@
                     </div>
                 </div>
             </div>
-            <div x-cloak class="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
+            <div x-cloak class="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
                 <button
                     class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
                     aria-label="Notifications">
@@ -93,21 +93,5 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div :class="{'block md:hidden': open, 'hidden': !open}" x-cloak>
-        @hasSection ('navbar-mobile')
-            @yield('navbar-mobile')
-        @else
-        <div class="px-2 pt-2 pb-3">
-            <a href="#"
-                class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
-            <a href="#"
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
-            <a href="#"
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
-            <a href="#"
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
-        </div>
-        @endif
     </div>
 </nav>
--- views/layout.blade.php
+++ views/layout.blade.php
@@ -1,11 +1,16 @@
-<div class="flex flex-wrap">
+<div class="flex flex-wrap" x-data='{open_sidebar: false}'>
     <div class="w-full">
         @include("paksuco-backend::header")
     </div>
-    <div class="w-12 lg:relative lg:block lg:w-1/5 xl:w-1/6 sidebar bg-gray-800">
-        @include("paksuco-backend::sidebar")
-    </div>
-    <div class="lg:w-4/5 xl:w-5/6 content">
-        @yield('body')
+    <div class="w-full relative flex ">
+        <div :class="{ 'block': open_sidebar, 'hidden lg:block': !open_sidebar }"
+             class="absolute inset-0  bg-gray-800 transform lg:relative lg:translate-x-0 lg:w-1/5 xl:w-1/6 sidebar z-10">
+            @include("paksuco-backend::sidebar")
+        </div>
+        <div class="w-full lg:w-4/5 xl:w-5/6 content z-0" x-cloak>
+            <div class="w-full px-5 py-4 text-xs bg-gray-200 border-b"><b>Home</b> > <b>Bread</b> > <b>Crumbs</b> > <a href="#">Here</a></div>
+            <div>@yield('body')</div>
+            <footer class="p-4 bg-gray-50 text-right text-sm">Created by Taha PAKSU ©{{\Carbon\Carbon::now()->year}}</footer>
+        </div>
     </div>
 </div>
