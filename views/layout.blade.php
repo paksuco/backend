97d721f85548d844b654a0471118b2c3e45b9d0c
Taha Paksu <taha.paksu@edata.com.tr>
2020-07-21 15:07:55 +0300

    Add button sidebar

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -1,15 +1,18 @@
-<div class="flex flex-wrap" x-data='{open_sidebar: false}'>
-    <div class="w-full z-0">
+<div class="flex flex-wrap layout" x-data='{open_sidebar: false}' :class="{ 'open_sidebar': open_sidebar, 'closed_sidebar': !open_sidebar }">
+    <div class="w-full z-20 bg-white shadow-md">
         @include("paksuco-backend::header")
     </div>
     <div class="w-full relative flex z-10">
         <div :class="{ 'block': open_sidebar, 'hidden lg:block': !open_sidebar }"
-             class="absolute inset-0  bg-cool-gray-800 transform lg:relative lg:translate-x-0 lg:w-1/5 xl:w-1/6 sidebar z-10">
+             class="absolute inset-0 bg-cool-gray-800 transform shadow lg:relative lg:translate-x-0 lg:w-1/5 xl:w-1/6 sidebar z-10">
             @include("paksuco-backend::sidebar")
         </div>
-        <div class="w-full lg:w-4/5 xl:w-5/6 content z-0 bg-cool-gray-100" x-cloak>
-            <div class="w-full px-5 py-3 text-xs bg-cool-gray-200 text-gray-900"><b>Home</b> >
-                <b>Bread</b> > <b>Crumbs</b> > <a href="#">Here</a></div>
+        <div class="w-full content z-0 bg-cool-gray-100" x-cloak :class="{'lg:w-4/5 xl:w-5/6': open_sidebar}">
+            <div class="w-full p-4">
+                <div class="w-full px-5 py-3 text-xs bg-cool-gray-50 rounded shadow text-gray-900">
+                    <b>Home</b> > <b>Bread</b> > <b>Crumbs</b> > <a href="#">Here</a>
+                </div>
+            </div>
             <div class="flex flex-col min-h-screen p-4">@yield('body')</div>
             <footer class="p-4 text-right text-sm">Created by Taha PAKSU
                 ©{{\Carbon\Carbon::now()->year}}</footer>
--- views/sidebar.blade.php
+++ views/sidebar.blade.php
@@ -1,1 +1,83 @@
+<nav x-show="open_sidebar">
 @yield("sidebar")
+</nav>
+<nav x-show="!open_sidebar">
+@yield("sidebar-alt")
+</nav>
+
+<style>
+    .layout.closed_sidebar .sidebar,
+    .layout.closed_sidebar .w-4 {
+        width: auto !important;
+    }
+/*
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu'] {
+        padding: 0;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a>div>span {
+        display: none;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a>div>i {
+        font-size: 26px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a>div {
+        line-height: 1;
+        font-size: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: 5px;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a>div:after {
+        content: "";
+        display: block;
+        padding-bottom: 100%;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a>i {
+        position: absolute;
+        top: 100%;
+        left: 100%;
+        display: flex;
+        align-items: end;
+        padding: 0;
+        transform: rotateZ(-45deg);
+        color: gold;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li>a {
+        padding: 0 !important;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li ul {
+        position: absolute;
+        top: 0;
+        left: 100%;
+        overflow: visible;
+        border-radius: 6px;
+        background: transparent !important;
+        box-shadow: 0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06);
+        border: none;
+    }
+
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li ul li {
+        border: none;
+        background: white;
+        border-radius: 0;
+    }
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li ul li:first-child {
+        border-radius: 6px 6px 0 0 !important;
+    }
+    .layout.closed_sidebar .sidebar [class*='paksuco-menu']>ul>li ul li:last-child {
+        border-radius: 0 0 6px 6px !important;
+    }*/
+</style>
